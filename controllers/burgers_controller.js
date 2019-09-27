7829c08e5824d061a382c70aeef4ca750fe1e6b2
Nermin Ceric <nerceric@gmail.com>
2019-09-26 23:08:37 -0500

    added some exports to js files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const burger = require("./models/burger.js");
+
+var router = express.Router();
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgername) VALUES ('McDouble');
+INSERT INTO burgers (burgername) VALUES ('QuarterPounder');
+INSERT INTO burgers (burgername) VALUES ('Whopper');
